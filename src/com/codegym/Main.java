1a46a4ae9dd46df6dbaefd75696ac5d598d83a0b
thaophamhetxat <thaophamhetxat@gmail.com>
2021-06-30 15:15:27 +0700

    valible email

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+public class Main {
+    private static EmailExample emailExample;
+    public static final String[] valiEmail = new String[]{"a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"};
+    public static final String[] invaliEmail = new String[]{"@gmail.com", "ab@gmail.", "@#abc@gmail.com"};
+
+
+    public static void main(String[] args) {
+        emailExample = new EmailExample();
+        for (String email : valiEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email +" is valid: "+ isvalid);
+        }
+        for(String email:invaliEmail){
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email +" is valid: "+ isvalid);
+        }
+    }
+
+}
